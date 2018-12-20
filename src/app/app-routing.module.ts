1e42828545b529d2cf3e0a5766274dd72fbba80b
tclostio <twclostio@gmail.com>
2018-12-19 21:24:07 -0800

    streamlined some navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {ProfilesComponent} from './profiles/profiles.component';
 import {ProfileComponent} from './profile/profile.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profiles', component: ProfilesComponent },
